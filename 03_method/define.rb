75dbb145868ab68a2a0500bb367eac1778b6b637
yokoto <youkotoushi@gmail.com>
2020-12-19 18:47:20 +0900

    solve 03_method

--- 03_method/define.rb
+++ 03_method/define.rb
@@ -1,6 +1,11 @@
 # Q1.
 # 次の動作をする A1 class を実装する
 # - "//" を返す "//"メソッドが存在すること
+class A1
+  define_method '//' do
+    "//"
+  end
+end
 
 # Q2.
 # 次の動作をする A2 class を実装する
@@ -11,8 +16,58 @@
 #   - 引数がnilの場合は、dev_teamメソッドを呼ぶこと
 # - また、2で定義するメソッドは以下を満たすものとする
 #   - メソッドが定義されるのは同時に生成されるオブジェクトのみで、別のA2インスタンスには（同じ値を含む配列を生成時に渡さない限り）定義されない
+class A2
+  def initialize(array)
+    array.each do |arg|
+      method_name = "hoge_#{arg}"
+      # コンストラクタの中で動的にメソッドを定義するので、
+      # 特異メソッドを定義する必要がある。
+      # https://github.com/meganemura/reading-metaprogramming-ruby/blob/6070de3ca9857a7ad346064d1ff29baec4842eaf/03_method/define.rb#L19-L26
+      # define_singleton_method(symbol) { ... } -> Symbol
+      # self に特異メソッド name を定義します。
+      # https://docs.ruby-lang.org/ja/latest/method/Object/i/define_singleton_method.html
+      self.define_singleton_method(method_name) do |n|
+        return dev_team if n.nil?
+
+        method_name * n
+      end
+    end
+  end
+
+  def dev_team
+    'SmartHR Dev Team'
+  end
+end
 
 # Q3.
 # 次の動作をする OriginalAccessor モジュール を実装する
 # - OriginalAccessorモジュールはincludeされたときのみ、my_attr_accessorメソッドを定義すること
 # - my_attr_accessorはgetter/setterに加えて、boolean値を代入した際のみ真偽値判定を行うaccessorと同名の?メソッドができること
+
+# NOTE: 分からなすぎたがp.169でそれっぽいコードを見つけた。
+module OriginalAccessor
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+    def my_attr_accessor(attribute)
+      define_method attribute do
+        instance_variable_get "@#{attribute}"
+      end
+      define_method "#{attribute}=" do |value|
+        # https://github.com/meganemura/reading-metaprogramming-ruby/blob/6070de3ca9857a7ad346064d1ff29baec4842eaf/03_method/define.rb#L50-L54
+        if [true, false].include?(value)
+          define_singleton_method("#{attribute}?") do
+            !!send(attribute)
+          end
+        end
+        instance_variable_set("@#{attribute}", value)
+      end
+      # # https://chaika.hatenablog.com/entry/2016/10/19/153728
+      # define_method "#{attribute}?" do |bool|
+      #   !!bool === bool
+      # end
+    end
+  end
+end
--- 03_method/try_over3_3.rb
+++ 03_method/try_over3_3.rb
@@ -6,11 +6,34 @@ TryOver3 = Module.new
 # - `test_` メソッドがこのクラスに実装されていなくても `test_` から始まるメッセージに応答することができる
 # - TryOver3::A1 には `test_` から始まるインスタンスメソッドが定義されていない
 
+# p.58とp.73参考
+class TryOver3::A1
+  def run_test
+    nil
+  end
+
+  def method_missing(name, *args)
+    # https://github.com/meganemura/reading-metaprogramming-ruby/blob/6070de3ca9857a7ad346064d1ff29baec4842eaf/03_method/try_over3_3.rb#L14-L18
+    if name =~ /^test_/
+      return run_test
+    end
+
+    super
+    # super if !self.methods.grep(/^test_(.*)/) { TryOver3::A1.define_component $1 }
+  end
+
+  # def self.define_component(method)
+  #   define_method(method) do
+  #     self.new.run_test
+  #   end
+  # end
+end
 
 # Q2
 # 以下要件を満たす TryOver3::A2Proxy クラスを作成してください。
 # - TryOver3::A2Proxy は initialize に TryOver3::A2 のインスタンスを受け取り、それを @source に代入する
 # - TryOver3::A2Proxy は、@sourceに定義されているメソッドが自分自身に定義されているように振る舞う
+# https://github.com/meganemura/reading-metaprogramming-ruby/blob/6070de3ca9857a7ad346064d1ff29baec4842eaf/03_method/try_over3_3.rb#L26-L53
 class TryOver3::A2
   def initialize(name, value)
     instance_variable_set("@#{name}", value)
@@ -18,11 +41,37 @@ class TryOver3::A2
   end
 end
 
+class TryOver3::A2Proxy
+  def initialize(instance)
+    @source = instance
+  end
+
+  private
+
+  def method_missing(name, *args)
+    if source.respond_to?(name)
+      return source.public_send(name, *args)
+    end
+
+    super
+  end
+
+  # p.65 respond_to?によって呼び出される。
+  # ゴーストメソッドがあればtrueを戻し、
+  # なければsuperを呼び出し、常にfalseを戻す
+  def respond_to_missing?(method, include_private = false)
+    source.respond_to?(method) || super
+  end
+
+  attr_reader :source
+end
+
 
 # Q3
 # 前回 OriginalAccessor の my_attr_accessor で定義した getter/setter に boolean の値が入っている場合には #{name}? が定義されるようなモジュールを実装しました。
 # 今回は、そのモジュールに boolean 以外が入っている場合には hoge? メソッドが存在しないようにする変更を加えてください。
 # （以下は god の模範解答を一部変更したものです。以下のコードに変更を加えてください）
+# https://github.com/meganemura/reading-metaprogramming-ruby/blob/6070de3ca9857a7ad346064d1ff29baec4842eaf/03_method/try_over3_3.rb#L59-L86
 module TryOver3::OriginalAccessor2
   def self.included(mod)
     mod.define_singleton_method :my_attr_accessor do |attr_sym|
@@ -31,11 +80,25 @@ module TryOver3::OriginalAccessor2
       end
 
       define_method "#{attr_sym}=" do |value|
-        if [true, false].include?(value) && !respond_to?("#{attr_sym}?")
-          self.class.define_method "#{attr_sym}?" do
-            @attr == true
+        if [true, false].include?(value)
+          if !respond_to?("#{attr_sym}?")
+            self.class.define_method "#{attr_sym}?" do
+              @attr == true
+            end
+          end
+        else
+          if respond_to?(attr_sym)
+            # このモジュールのインスタンスメソッド name を未定義にします。
+            # undef_method(*name) -> self
+            # スーパークラスに同名のメソッドがあっても
+            # その呼び出しはエラーになる
+            self.class.undef_method(attr_sym)
+          end
+          if respond_to?("#{attr_sym}?")
+            self.class.undef_method("#{attr_sym}?")
           end
         end
+
         @attr = value
       end
     end
@@ -48,7 +111,33 @@ end
 # TryOver3::A4.runners = [:Hoge]
 # TryOver3::A4::Hoge.run
 # # => "run Hoge"
+# https://github.com/meganemura/reading-metaprogramming-ruby/blob/6070de3ca9857a7ad346064d1ff29baec4842eaf/03_method/try_over3_3.rb#L94-L114
+class TryOver3::A4
+  def self.runners=(args)
+    @runners = args
+  end
 
+  def self.runners
+    @runners
+  end
+
+  # p.66 存在しない定数を参照すると、Rubyは定数の名前を  const_missing にシンボルとして渡す。
+  # クラス名は単なる定数なので、ここではHogeという不明な参照が Module#const_missing に渡される。
+  def self.const_missing(const_name)
+    return super unless self.runners.include?(const_name)
+
+    # そして、runnersが定数への参照を持っていれば、run という名前の特異メソッドを定義する。
+    klass = Class.new do |c|
+      c.define_singleton_method "run" do
+        "run #{const_name}"
+      end
+    end
+
+    # const_set(name, value) -> object
+    # モジュールに name で指定された名前の定数を value という値として定義し、value を返します。
+    const_set(const_name, klass)
+  end
+end
 
 # Q5. チャレンジ問題！ 挑戦する方はテストの skip を外して挑戦してみてください。
 #
